8298d355c7ee14090b45cabbe9ed57d0a1629097
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-12 13:38:09 +0300

    Example004_Div Real

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA= new Random().Next(0,100);
+double numberB =new Random().Next(0,100);
+System.Console.WriteLine($"{numberA} / {numberB} = {numberA/numberB}");
