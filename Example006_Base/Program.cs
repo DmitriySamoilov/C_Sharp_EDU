6ce7e50fade6fa6f3926c254dbe2a65dd24458fa
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-12 14:45:03 +0300

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Clear();
+int a= new Random().Next(1,50);
+int b= new Random().Next(1,50);
+int c= new Random().Next(1,50);
+int d= new Random().Next(1,50);
+int e= new Random().Next(1,50);
+int max=a; 
+if (b> max) max=b;
+if (c> max) max=c;
+if (d> max) max=d;
+if (e> max) max=b;
+System.Console.WriteLine($"Гиря a= {a}");
+System.Console.WriteLine($"Гиря b= {b}");
+System.Console.WriteLine($"Гиря c= {c}");
+System.Console.WriteLine($"Гиря d= {d}");
+System.Console.WriteLine($"Гиря e= {e}");
+System.Console.WriteLine($"Наибольший вес {max}");
