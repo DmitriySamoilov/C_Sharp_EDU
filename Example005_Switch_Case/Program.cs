2f73d2c3d89f7df3dcb28e6e6215eb742381c774
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-12 14:15:50 +0300

    Example005_Switch_Case

---
+++ Example005_Switch_Case/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+System.Console.Write("Введите Ваше имя: ");
+string ? username = Console.ReadLine();
+
+switch(username.ToLower() ){
+case "маша":
+    System.Console.WriteLine($"Привет, дорогая Маша !");
+break;
+case "даша":
+    System.Console.WriteLine($"Привет велиокая Даша !");
+break;
+case "саша":
+    System.Console.WriteLine($"Привет полногрудая Саша !");
+break;
+case "глаша":
+    System.Console.WriteLine($"Привет челом красная Глаша !");
+break;
+default:
+    System.Console.WriteLine($"Привет незнакомая {username} !");
+break;
+
+}
