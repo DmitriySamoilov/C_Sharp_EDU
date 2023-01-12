08ca26a81d5d2387d0bd0f49c15c8eee55c6690e
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-12 20:48:20 +0300

    Example008_Two_man_and_dog

---
+++ Example008_Two_man_and_dog/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+double distance= 10000;
+int firstManSpeed= 1;
+int secondManSpeed=2;
+int dogSpeed= 5;
+bool direct = true;
+System.Console.WriteLine($"Расстояние между друзьми: {distance}");
+System.Console.WriteLine($"Скорость первого друга: {firstManSpeed}");
+System.Console.WriteLine($"Скорость второго друга: {secondManSpeed}");
+System.Console.WriteLine($"Скорость собаки: {dogSpeed}");
+System.Console.WriteLine("Движение навстречу");
+double time=0;
+int count=0;//Сколько раз пробежала собака
+while (distance>10) {
+    if (direct) 
+    {   time=distance/(secondManSpeed+dogSpeed);
+        System.Console.WriteLine($"Time: {time}");
+        direct=false;
+    } 
+    else {
+        time = distance/(firstManSpeed+dogSpeed);
+        System.Console.WriteLine($"Time: {time}");
+        direct=true;
+        }
+distance=distance-(firstManSpeed+secondManSpeed)*time;
+System.Console.WriteLine($"Distance: {distance}");
+count++;
+System.Console.WriteLine($"Собака пробежала: {count} раз");
+}
