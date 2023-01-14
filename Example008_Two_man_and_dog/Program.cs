1b7edc686c23d6b522f66bf08e94904b203618c2
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-14 13:54:38 +0300

    Example008_Two_man_and_dog + comments

--- Example008_Two_man_and_dog/Program.cs
+++ Example008_Two_man_and_dog/Program.cs
@@ -1,30 +1,35 @@
 ﻿Console.Clear();
 
-double distance= 10000;
-int firstManSpeed= 1;
-int secondManSpeed=2;
-int dogSpeed= 5;
-bool direct = true;
-System.Console.WriteLine($"Расстояние между друзьми: {distance}");
+double distance = 10000;
+int firstManSpeed = 1;
+int secondManSpeed = 2;
+int dogSpeed = 5;
+bool direct = true; //направление движения собаки. true - от первого ко второму. false- от второго к первому
+
+System.Console.WriteLine($"Расстояние между друзьями: {distance} метров");
 System.Console.WriteLine($"Скорость первого друга: {firstManSpeed}");
 System.Console.WriteLine($"Скорость второго друга: {secondManSpeed}");
 System.Console.WriteLine($"Скорость собаки: {dogSpeed}");
 System.Console.WriteLine("Движение навстречу");
-double time=0;
-int count=0;//Сколько раз пробежала собака
-while (distance>10) {
-    if (direct) 
-    {   time=distance/(secondManSpeed+dogSpeed);
-        System.Console.WriteLine($"Time: {time}");
-        direct=false;
-    } 
-    else {
-        time = distance/(firstManSpeed+dogSpeed);
-        System.Console.WriteLine($"Time: {time}");
-        direct=true;
-        }
-distance=distance-(firstManSpeed+secondManSpeed)*time;
-System.Console.WriteLine($"Distance: {distance}");
-count++;
-System.Console.WriteLine($"Собака пробежала: {count} раз");
+
+double time = 0;
+int count = 0;//Сколько раз пробежала собака
+while (distance > 10)
+{
+    if (direct)
+    {
+        time = distance / (secondManSpeed + dogSpeed);
+        direct = false;                                 //смена направления движения собаки
+    }
+    else
+    {
+        time = distance / (firstManSpeed + dogSpeed);
+        direct = true;                                  //смена направления движения собаки
+    }
+    System.Console.WriteLine($"Время бега собаки: {Math.Round(time)} секунды");
+    distance = distance - (firstManSpeed + secondManSpeed) * time;
+    System.Console.WriteLine($"Остаток расстояния между друзьями: {Math.Round(distance)} метров");
+    count++;
+    System.Console.WriteLine($"Собака пробежала: {count} раз");
 }
+System.Console.WriteLine("Конец.");
