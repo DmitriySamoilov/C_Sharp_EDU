c9ad556bcce9f90d934073243ef2424e2d479a64
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-17 22:56:05 +0300

    Задачи семинара

---
+++ Example011_Array_Library/Program.cs
@@ -0,0 +1,46 @@
+﻿//Console.Clear();
+
+void FillArray(int[] array)
+{
+    int index = 0;
+    while (index < array.Length)
+    {
+        array[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int index = 0;
+    while (index < array.Length)
+    {
+        System.Console.Write($" {array[index]} ");
+        index++;
+    }
+}
+
+int Indexof(int[] array, int findElement)
+{
+    int index = 0;
+    int position = -1;
+    bool keepLoop=true;
+    while ((index < array.Length)&&(keepLoop))
+    {
+        if (array[index] == findElement)
+        {
+            position = index;
+            keepLoop=false;              //Прерывает цикл
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+int findIndex = Indexof(array, 5);
+System.Console.WriteLine();
+System.Console.WriteLine(findIndex);
---
+++ Example012/Program.cs
@@ -0,0 +1,26 @@
+﻿double newNumber()
+{
+    System.Console.Write("-->");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+
+/*double first=newNumber();
+double second=newNumber();
+if (first%second==0){
+    System.Console.WriteLine(first+" Кратно "+second);
+}
+else System.Console.WriteLine("Некратно остаток "+(first%second));*/
+
+
+double num = newNumber();
+if ((num % 7 == 0) && (num % 23 == 0))
+{
+    System.Console.WriteLine(num + " Кратно 7 и 23 ");
+
+}
+else
+{
+    System.Console.WriteLine(num + " Некратно");
+}
