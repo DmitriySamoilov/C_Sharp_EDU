c54ac5ad5fa3ed71ba904614b4e4722413e7bc68
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-01-20 10:47:07 +0300

    Семинар3

---
+++ Example013/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] array =new int[4,3];
+
+
+Random random= new Random();
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,] array)
+{
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        array[i,j]=random.Next(99,1000);
+    }
+}
+}
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i < 4; i++)
+{
+    System.Console.WriteLine();
+    for (int j = 0; j < 3; j++)
+    {
+       System.Console.Write($" {array[i,j]} ");
+    }
+}
+}
+
