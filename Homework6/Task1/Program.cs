341d06e387d973610a28161a68ba4d1a06ec0e71
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-02-02 20:58:22 +0300

    Homework6

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int [] StringToIntArray(string sourceString) // Исходный string расскладываем на int массив по набору разделителей
+{
+    char[] separators = new char []{' ',',',';','/','.'};
+    string[] arrayString=sourceString.Split(separators,StringSplitOptions.RemoveEmptyEntries);
+    int[] arrayInt= Array.ConvertAll(arrayString,int.Parse);
+    return  arrayInt;
+}
+void PrintArray(int [] array)   //Печатаем массив для проверки
+{
+foreach (int item in array)
+{
+    System.Console.Write($"{item} ");
+}
+}
+
+System.Console.Write("Input integer numbers: ");
+string ? inputedString=Console.ReadLine();
+if (inputedString==null) {System.Console.WriteLine("Something is going wrong!"); return;}
+
+int [] array= StringToIntArray( inputedString);
+
+PrintArray(array);
+
+int count=0;
+foreach (int item in array)
+{
+    if (item>0) count++;
+}
+
+System.Console.WriteLine($"\n {count} more than 0 numbers");
+
+
+
+
+
---
+++ Homework6/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//Применил более общий вид формулы для прямых на плоскости ax+by=c. Позволяет дополнительно учитывать прямые параллельные оси Y (Плачь, Камянецкий!!!)
+//Для исходного примера из условий задачи данные будут соответствовать: a1=-5, b1=1, c1=2 и a2=-9, b2=1, c2=4 -> (-0,5; -0,5)
+//Для прямых параллельных оси X: a=0, b=1 или любое число  кроме 0(?), c=любое число.
+//Для прямых параллельных оси Y: a=1 или любое число кроме 0(?), b=0, c=любое число.
+
+void FindIntersectionPointCoordinates(double a1, double a2, double b1, double b2, double c1, double c2, out double xCrossPoint, out double yCrossPoint) 
+{
+    xCrossPoint = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+    yCrossPoint = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+}
+
+double InputDoubleNumber(string numberName)
+{
+    System.Console.Write($"Input {numberName} :");
+    double doubleNumber = Convert.ToDouble(Console.ReadLine());
+    return doubleNumber;
+}
+System.Console.WriteLine("Use a straight line type ax+by=c!");
+double a1 = InputDoubleNumber("a1");
+double b1 = InputDoubleNumber("b1");
+double c1 = InputDoubleNumber("c1");
+if (a1==0 && b1==0) { System.Console.WriteLine($"The straight line type {a1}x + {b1}y={c1} does not exist."); return;}
+double a2 = InputDoubleNumber("a2");
+double b2 = InputDoubleNumber("b2");
+double c2 = InputDoubleNumber("c2");
+if (a2==0 && b2==0) { System.Console.WriteLine($"The straight line type {a2}x + {b2}y={c2} does not exist."); return;}
+
+System.Console.WriteLine($"The first  straight line: {a1}x + {b1}y={c1};");
+System.Console.WriteLine($"The second straight line: {a2}x + {b2}y={c2};");
+
+if ((a1 * b2 - a2 * b1) == 0)
+{
+    System.Console.WriteLine("The Parallel lines. The intersection point does not exist.");
+    return;
+}
+double xIntersectionPointCoordinate = 0;
+double yIntersectionPointCoordinate = 0;
+FindIntersectionPointCoordinates(a1: a1, a2: a2, b1: b1, b2: b2, c1: c1, c2: c2, out xIntersectionPointCoordinate, out yIntersectionPointCoordinate);
+
+System.Console.WriteLine($"Coordinates of the intersection point ({xIntersectionPointCoordinate:f2};{yIntersectionPointCoordinate:f2})");
