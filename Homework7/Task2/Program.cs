132c836f5d25e6fa40e0c32afd5febd9033e2d8b
Dmitriy Samoilov <ddd2008ok@gmail.com>
2023-02-04 20:42:29 +0300

    Homework7

---
+++ Homework7/Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int InputIntNumber(string numberName)
+{
+    System.Console.Write($"Input {numberName} :");
+    int intNumber = Convert.ToInt32(Console.ReadLine());
+    return intNumber;
+}
+
+void FillIntArray2Drandom(int[,] array, Random rnd, int min, int max)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+}
+
+void PrintIntArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//Игра Морской бой (шутка)
+
+int[,] array = new int[10, 10];
+Random rnd = new Random();
+FillIntArray2Drandom(array, rnd, 10, 30);
+System.Console.WriteLine("Сыграем в игру?");
+bool keepLooping = true;
+while (keepLooping)
+{
+    int rowNumber = InputIntNumber("number of row");
+    int columnNumber = InputIntNumber("number of column");
+    if (rowNumber >= array.GetLength(0) || columnNumber >= array.GetLength(1) || rowNumber < 0 || columnNumber < 0) //Индекс за пределами массива. Выход
+    {
+        System.Console.WriteLine($"Выстрел...Вы улетели в космос. Такого элемента нет.");
+        keepLooping = false;
+    }
+    //Проверяем и выводим значение
+    else if (array[rowNumber, columnNumber] % 5 == 0) System.Console.WriteLine($"Выстрел... Убит. [{rowNumber},{columnNumber}] = {array[rowNumber, columnNumber]}");
+    else if (array[rowNumber, columnNumber] % 3 == 0) System.Console.WriteLine($"Выстрел... Ранен. [{rowNumber},{columnNumber}] = {array[rowNumber, columnNumber]}");
+    else System.Console.WriteLine($"Выстрел... В молоко. [{rowNumber},{columnNumber}] = {array[rowNumber, columnNumber]}");
+}
+PrintIntArray2D(array);
---
+++ Homework7/Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int InputIntNumber(string numberName)
+{
+    System.Console.Write($"Input {numberName} :");
+    int intNumber = Convert.ToInt32(Console.ReadLine());
+    return intNumber;
+}
+
+void FillIntArray2Drandom(int[,] array, Random rnd, int min, int max)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+}
+
+void PrintIntArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PrintColumnsAverageArray2D(int[,] array)
+{
+    double columnSum;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   columnSum=0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            columnSum=columnSum+array[j, i];
+        }
+        System.Console.Write($"{columnSum/array.GetLength(0) :f2};\t");
+    }
+}
+
+int m = InputIntNumber("number of rows:");
+int n = InputIntNumber("number of columns:");
+int[,] intArray2D = new int[m, n];
+Random rnd = new Random();
+FillIntArray2Drandom(intArray2D,rnd,1,9);
+PrintIntArray2D(intArray2D);
+PrintColumnsAverageArray2D(intArray2D);
